b97acdd2b3e9208c63288abbca089892c53a879b
Otwrate <Otwrate@gmail.com>
2018-04-12 18:24:23 +0200

    CaptureFrame Update

--- capture.py
+++ capture.py
@@ -8,12 +8,11 @@ Created on Fri Mar 30 12:37:14 2018
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import multiprocessing
 
 def CaptureFrame ():
-    i = 0
-    frame_list = []
     V_list = []
-    
+    IsCaptured = 0
     
     #Choosing source of input
     
@@ -22,25 +21,27 @@ def CaptureFrame ():
     cap = cv2.VideoCapture("ankieta_OK.avi"); 
     
     while(True):
-        i += 1
     #   skoro chcecie kazda ramke
     #    if  not(i % 5): 
         ret, frame = cap.read()
         if ret == 1 :
-    
+            
             #Converting image to HSV
             frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             V = np.mean(frame_HSV[:,:,2])
             #Aggregating V-values
             V_list.append(V)
-            if V>80 and i>10 and np.sum(np.square(V - V_list[-10:-2]))<1:
-                break
-    
+            if IsCaptured == 0 and V>80 and i>10 and np.sum(np.square(V - V_list[-10:-2]))<1:
+                #pipe.send(frame)
+                IsCaptured = 1
+            if IsCaptured == 1 and V < 10:
+                IsCaptured = 0
+                
         else:
             break
             
     #plt.plot(V_list)
     cap.release()
     cv2.destroyAllWindows()
-    return frame
+    
 
